3de8efd3ad1ba2d24bf5a9f1ba2036d24f3aa22a
rb_ <41527196+EndaHallahan@users.noreply.github.com>
2019-11-14 13:37:06 -0600

    Updated documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,8 @@ BBClash is the open-source version of the BBCode compiler being built for [Pencl
 
 Our BBCode specification can be found [here](https://github.com/EndaHallahan/BBClash/blob/master/Spec.md).
 
+*Note: currently requires Rust Nightly to build. Relevant issue: [54727](https://github.com/rust-lang/rust/issues/54727)*
+
 ## General Usage:
 
 ```rust
@@ -45,7 +47,7 @@ Note that neither mode arbitrarily strips any text in square brackets. This only
 
 ## Custom Usage:
 
-Because this package was built for an existing application, and because it is performance-focused, BBClash's BBCode implementation is entirely hard-coded. Because of this, it is reccommended that you download a local copy and modify it to suit your needs. *Note: currently requires Rust Nightly to build. Relevant issue: [54727](https://github.com/rust-lang/rust/issues/54727)*
+Because this package was built for an existing application, and because it is performance-focused, BBClash's BBCode implementation is entirely hard-coded. Because of this, it is reccommended that you download a local copy and modify it to suit your needs. 
 
 Building is as simple as running `$ cargo build`. Tests and benchmarks can be run with `$ cargo test` and `$ cargo bench`, respectively.
 */
